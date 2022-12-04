7d84c62525086ceb1a36b14c7fc7fd28302eeea2
Anothermi <104280247+Anothermi1@users.noreply.github.com>
2022-12-04 19:30:25 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -9,7 +9,6 @@ function init() {
 }
 # Main 
 function compile() {
-    export PATH="/home/romiyusnandar/toolchains/proton-clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 <DEVICE>_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CROSS_COMPILE=aarch64-linux-gnu- \
