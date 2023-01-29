6b0cf32b489628c0b6a93108bf776f7b5d5ce546
Romiyus <104280247+Anothermi1@users.noreply.github.com>
2023-01-29 17:46:36 +0700

    Update compile_with_tc.sh
    fix clang not imported when compile kernel

--- compile_with_tc.sh
+++ compile_with_tc.sh
@@ -12,6 +12,7 @@ function compile() {
     export PATH="/home/romiyusnandar/toolchains/proton-clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 <DEVICE>_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
+                          CC=clang \
                           CROSS_COMPILE=aarch64-linux-gnu- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 }
