be8a058b08ef0575d06804e07dde1f2762db424f
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2023-11-15 10:23:20 +0000

    Add script for compile gki kernel

---
+++ compile_gki.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#
+# Copyright (C) 2023 sirNewbies
+#
+
+# init
+WORK_DIR=$(pwd)
+ANYKERNEL=$WORK_DIR/anykernel
+ANYKERNEL_REPO="https://github.com/sirnewbies/Anykernel3.git" 
+ANYKERNEL_BRANCH="topaz"
+KERNEL_DIR="topaz"
+
+# VERSIONING
+REL="v1.6"
+KERNEL="QuantumCharge-topaz-tapas-$REL"
+ZIPNAME=$KERNEL.zip
+KERN_IMG=$WORK_DIR/out/android13-5.15/dist/Image
+
+# setup telegram
+CHATIDQ="-1001930168269"
+CHATID="-1001930168269" # Group/channel chatid (use rose/userbot to get it)
+TELEGRAM_TOKEN="5136791856:AAGY5TeaVoeJbd6a2BAlxAjOc-MFWOJzZds" # Get from botfather
+
+# Export Telegram.sh
+TELEGRAM_DIR=$WORK_DIR/telegram
+if ! [ -d "$TELEGRAM" ]; then
+    git clone https://github.com/sirnewbies/telegram.sh/ "${TELEGRAM}"
+fi
+
+TELEGRAM=$TELEGRAM_DIR/telegram
+
+# setup color
+red='\033[0;31m'
+green='\e[0;32m'
+white='\033[0m'
+yellow='\033[0;33m'
+
+function clean() {
+    echo -e "$red << cleaning up... >> \n$white"
+    rm -rf $ANYKERNEL out
+}
+
+function update_ksu() {
+    echo -e "$yellow << updateing kernelsu.. >> \n$white"
+    cd $KERNEL_DIR || exit
+    ./update_ksu.sh
+}
+
+function pack_kernel() {
+    echo -e "$yellow << packing kernel... >> \n$white"
+
+    git clone "$ANYKERNEL_REPO" -b "$ANYKERNEL_BRANCH" "$ANYKERNEL"
+
+    cp $KERN_IMG $ANYKERNEL/Image
+    cd $ANYKERNEL || exit
+    zip -r9 $ZIPNAME ./*
+
+    $TELEGRAM -f $ZIPNAME -t $TELEGRAM_TOKEN -c $CHATIDQ
+
+    echo -e "$green << kernel uploaded to telegram >>"
+}
+
+function build_kernel() {
+    echo -e "$yrllow << building kernel... >> \n$white"
+    cd $WORK_DIR
+    LTO=thin BUILD_CONFIG=$KERNEL_DIR/build.config.gki.aarch64 build/build.sh
+
+    if [ -e "$KERN_IMG" ]; then
+        echo -e "$green << compile kernel success! >> \n$white"
+        pack_kernel
+    else
+        echo -e "$red << compile kernel failed! >> \n$white"
+    fi
+}
+
+# exe
+clean
+update_ksu
+build_kernel
