cd0f148c928278efc6f7ba1cd768ddd85d32d733
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2023-12-01 05:07:23 +0700

    Make script more better

--- compile_with_tc.sh
+++ compile_with_tc.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
-# Copyright cc 2022 anothermi
+# Copyright cc 2023 sirnewbies
 
-# init
-function init() {
-    echo "==========================="
-    echo "= START COMPILING KERNEL  ="
-    echo "==========================="
+# setup color
+red='\033[0;31m'
+green='\e[0;32m'
+white='\033[0m'
+yellow='\033[0;33m'
+
+# setup dir
+WORK_DIR=$(pwd)
+KERN_IMG="${WORK_DIR}/out/arch/arm64/boot/Image-gz.dtb"
+
+function clean() {
+    echo -e "\n"
+    echo -e "$red << cleaning up >> \n$white"
+    echo -e "\n"
+    rm -rf out
 }
-# Main 
-function compile() {
+
+function build_kernel() {
     export PATH="/home/romiyusnandar/toolchains/proton-clang/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 <DEVICE>_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CC=clang \
                           CROSS_COMPILE=aarch64-linux-gnu- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-}
-#end
-function ended(){
-    echo "==========================="
-    echo "   COPILE KERNEL COMPLETE  "
-    echo "==========================="
+    if [ -e "$KERN_IMG" ]; then
+        echo -e "\n"
+        echo -e "$green << compile kernel success! >> \n$white"
+        echo -e "\n"
+    else
+        echo -e "\n"
+        echo -e "$red << compile kernel failed! >> \n$white"
+        echo -e "\n"
+    fi
 }
 
 # execute
-init
-compile
-ended
+clean
+build_kernel
