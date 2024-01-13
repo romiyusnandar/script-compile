5f8cebac65a429844be03abf045ba5edd367204b
sirNewbies <104280247+sirnewbies@users.noreply.github.com>
2024-01-13 20:43:07 +0700

    Update compile_with_gcc.sh

--- compile_with_gcc.sh
+++ compile_with_gcc.sh
@@ -26,7 +26,8 @@ function build_kernel() {
     make -j$(nproc --all) O=out ARCH=arm64 <DEVICE>_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
                           CROSS_COMPILE=aarch64-linux-gnu- \
-                          CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+                          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                          CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
                           
     if [ -e "$KERN_IMG" ] || [ -e "$KERN_IMG2" ]; then
         echo -e "\n"
